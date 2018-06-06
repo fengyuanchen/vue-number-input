d14af4611b182615298e841db5806e00fac34615
Chen Fengyuan <chenfengyuan@outlook.com>
2018-06-06 21:47:06 +0800

    fix: off auto complete

--- src/index.vue
+++ src/index.vue
@@ -6,7 +6,7 @@
     [`number-input--${size}`]: size,
   }" v-on="listeners">
     <button v-if="controls" class="number-input__button number-input__button--minus" type="button" @click="decrease" :disabled="disabled || readonly || !decreasable"></button>
-    <input class="number-input__input" ref="input" type="number" :name="name" :value="currentValue" :min="min" :max="max" :step="step" @change="change" @paste="paste" :readonly="readonly || !inputtable" :disabled="disabled || (!decreasable && !increasable)">
+    <input class="number-input__input" ref="input" type="number" :name="name" :value="currentValue" :min="min" :max="max" :step="step" @change="change" @paste="paste" :readonly="readonly || !inputtable" :disabled="disabled || (!decreasable && !increasable)" autocomplete="off">
     <button v-if="controls" class="number-input__button number-input__button--plus" type="button" @click="increase" :disabled="disabled || readonly || !increasable"></button>
   </div>
 </template>
