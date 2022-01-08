60d69f2421227f12a5fe5b4a28d3434938b6f61f
Chen Fengyuan <chenfengyuan@outlook.com>
2022-01-08 14:02:16 +0800

    build: update dependencies and configurations

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recommended-scss',
+    'stylelint-config-recommended-vue/scss',
   ],
   plugins: [
     'stylelint-order',
